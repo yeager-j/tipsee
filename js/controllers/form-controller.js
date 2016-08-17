65f9743352a7b57b4b4b3f8e38d7b5ffef9a8a90
TheNew000 <mahkatorocks@gmail.com>
2016-08-17 17:38:25 -0400

    created form-controller

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,9 @@
+app.controller('formCtrl', function($scope){
+    $scope.user = {
+        name: 'Name Here',
+        date: 'Last date worked',
+        tip: 'Tip Amount',
+        basepay: 'Shift Pay',
+        hours: 'Hours Worked'
+    };
+});
