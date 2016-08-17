8abe655a570d20e125c72d4611352927ac024b41
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-17 16:52:13 -0400

    Added readme

---
+++ js/data/data.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Jackson on 8/17/16.
+ */
+
+var tipData = {
+    danny: {
+        day1: {
+            tipAmount: 2.00,
+            basePay: 120.00,
+            hours: 10,
+            date: Date.now()
+        },
+        day2: {
+            tipAmount: 3.00,
+            basePay: 120.00,
+            hours: 11,
+            date: Date.now()
+        }
+    },
+    summer: {
+        day1: {
+            tipAmount: 2.50,
+            basePay: 125.00,
+            hours: 24,
+            date: Date.now()
+        }
+    }
+};
---
+++ readme.md
@@ -0,0 +1,20 @@
+#Tipsee
+A one stop app for all work-related tipping
+
+## Data format
+**Input:**
+- Tip amount
+- Base pay
+- Hours
+- Date
+
+
+##Features
+- Work schedule
+    - Recurring events
+- Collaboration
+    - Chat with employees
+    - Master calendar
+- Data
+    - Graphs
+    - Spreadsheets
