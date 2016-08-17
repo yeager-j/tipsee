ad1d4d5711a287e941923404bd5fc2c9839091d8
summeradhal <summeradhal@gmail.com>
2016-08-17 16:24:52 -0400

    what up

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -2,5 +2,5 @@
  * Created by Jackson on 8/17/16.
  */
 app.controller('mainCtrl', function($scope){
-    $scope.hello = "Hello World! Hello, Danny! Hello Summer! Howdy Jackson, Howdy Summer!";
+    $scope.hello = "Hello World! Hello, Danny! Hello Summer! Howdy Jackson, Howdy Summer! What up";
 });
