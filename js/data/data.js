4ebc1f46d380c8566b0ba2e1cd5a6e983d92dc41
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-23 11:10:39 -0400

    Added more data, added fixed thead

--- js/data/data.js
+++ js/data/data.js
@@ -17,27 +17,13 @@ var tipData = [
     }
 ];
 
-for(var i = 0; i < 10; i++){
-    tipData[0].days.push({
-        date: Date.now() + 1000 * 24 * 60 * 60 * i,
-        tipAmount: Math.random() * 15,
-        basePay: 120,
-        hours: 8
-    });
-
-    tipData[1].days.push({
-        date: Date.now() + 1000 * 24 * 60 * 60 * i,
-        tipAmount: Math.random() * 15,
-        basePay: 120,
-        hours: 8
-    });
-
-    tipData[2].days.push({
-        date: Date.now() + 1000 * 24 * 60 * 60 * i,
-        tipAmount: Math.random() * 15,
-        basePay: 120,
-        hours: 8
-    });
-
-    console.log(Date.now() + 24 * 60 * 60 * i);
+for(var i = 0; i < 50; i++){
+    for(var j = 0; j < 3; j++){
+        tipData[j].days.push({
+            date: Date.now() + 1000 * 24 * 60 * 60 * i,
+            tipAmount: Math.random() * 15,
+            basePay: 120,
+            hours: 8
+        });
+    }
 }
