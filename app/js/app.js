a50ea0f45ea6181780632c191f36510e404a4ae8
TheNew000 <mahkatorocks@gmail.com>
2016-08-25 16:40:56 -0400

    Began hand built graph functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Jackson on 8/17/16.
  */
-app = angular.module('tipsee', ['ngRoute', 'ngCookies','mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module', 'gridshore.c3js.chart']);
+app = angular.module('tipsee', ['ngRoute', 'ngCookies','mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module', 'angular-c3-simple']);
 
 app.config(function($routeProvider){
     $routeProvider
