172659875f43393013399999b1141229ef1f4bf1
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-25 04:51:43 -0400

    Revert "Data graphs"

--- js/controllers/graph-controller.js
+++ js/controllers/graph-controller.js
@@ -1,12 +1,17 @@
-app.controller('graphCtrl', function($scope, $interval, dataService){
-    $scope.datapoints=[];
-    $scope.datacolumns=[{"id":"top-1","type":"spline","name":"Danny","color":"green"},
-                        {"id":"top-2","type":"spline","name":"Summer","color":"red"}];
+app.controller('graphCtrl', function($scope){
+    $scope.datapoints=[{"x":10,"top-1":10,"top-2":15},
+                       {"x":20,"top-1":100,"top-2":35},
+                       {"x":30,"top-1":15,"top-2":75},
+                       {"x":40,"top-1":50,"top-2":45}];
+    $scope.datacolumns=[{"id":"top-1","type":"line"},
+                        {"id":"top-2","type":"spline"}];
     $scope.datax={"id":"x"};
 
-    $interval(function(){
-        dataService.loadData(function(data){
-            $scope.datapoints.push(data);
-        });
-    },1000,10);
+    $scope.datapoints2=[{"x":10,"top-1":10,"top-2":15},
+                       {"x":20,"top-1":100,"top-2":35},
+                       {"x":30,"top-1":15,"top-2":75},
+                       {"x":40,"top-1":50,"top-2":45}];
+    $scope.datacolumns2=[{"id":"top-1","type":"line","name":"Top one","color":"green"},
+                        {"id":"top-2","type":"spline","name":"Top two","color":"blue"}];
+    $scope.datax2={"id":"x"};
 });
