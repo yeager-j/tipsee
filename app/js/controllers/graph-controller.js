23d2df78b6b5f97e33c9380709e48d6981fa1b91
TheNew000 <mahkatorocks@gmail.com>
2016-08-26 14:07:08 -0400

    Made the Graph dynamic and started adding commentary on our file

--- app/js/controllers/graph-controller.js
+++ app/js/controllers/graph-controller.js
@@ -1,46 +1,107 @@
-app.controller('graphCtrl', [$scope, c3SimpleService, function($scope, c3SimpleService) {
-    $scope.chart = {
+app.controller('graphCtrl', ['$scope', 'c3SimpleService', function($scope, c3SimpleService) {
+// Fine Tuning of the chart object
+    var chart = {
+        // Data to populate chart
         data: {
-        columns: [
-            ['Danny', 30, 200, 100, 400, 150, 250],
-            ['Jackson', 50, 20, 10, 40, 15, 25],
-            ['Summer', 50, 20, 10, 40, 15, 25]
-          ],
-        axes: {
-            Jackson: 'y2',
-            Summer: 'y2'
-          },
-        types: {
-            Jackson: 'bar',
-            Danny: 'y2',
-            Summer: 'y2'
-          }
+            x: 'x',
+            columns: [],
+            types: {},
+            groups: [['Total Emp Take', 'Daily Total']]
         },
+        // Adjust the stlyes of the axis's
         axis: {
+            x: {
+                label: {
+                    text: 'Day\'s Worked',
+                    position: 'outer-left',
+                },
+                type: 'timeseries',
+                tick:{
+                    format: '%a-%d-%b',
+                    rotate: 10
+                }
+            },
           y: {
             label: {
-              text: 'Y Label',
+              text: 'Money Made',
               position: 'outer-middle'
-            }
-          },
-          y2: {
-            show: true,
-            label: {
-              text: 'Y2 Label',
-              position: 'outer-middle'
+            },
+            tick: {
+                format: d3.format('$'),
             }
           }
         },
+        // Chart Padding
+        padding:{
+            bottom: 40
+        },
+        // Tooltip Formatting
         tooltip: {
             format: {
-                title: function(x){return 'Today\'s Date ' + x;},
-                name: function (name, ratio, id, index){return "On " + index + " " + name + "made:";},
+                name: function (name, ratio, id, i){ return name + " made:";},
                 value: function (value, ratio, id) {
                     var format = d3.format('$');
                     return format(value);
                 }
             }
-        }     
+        },
+        // Show Sub Chart
+        subchart: {
+            show: true
+        },
+        legend: {
+            position: 'right'
+        },
+        zoom: {
+            rescale: true
+        }  
     }
+    // Laying out our variables (mostly arrays) we will fill with the subsequent data
+    $scope.chart = chart;
+    var dailyBP = [];
+    var dailyTip = [];
+    var dailyTotal = [];
+    var current = null;
+    var dataArr = {};
+    var totalsArr = ['Total Emp Take'];
+
+    // Populate Our chart variable with relevant Data
+    // Make the "X" array more dynamic
+        for(var i = 0; i < tipData.length; i++){
+            for(var j = 0; j < tipData[i].days.length; j++){
+                // Push each persons tip into their own array
+                tipData.forEach(function(i){         
+                    if(!Array.isArray(dataArr[i.name])){
+                        dataArr[i.name] = [i.name];
+                    }
+                    dataArr[i.name].push(i.days[j].tipAmount);
+                    // Just for fun I'm including another array to show a stacked bar
+                    totalsArr.push(i.days[j].tipAmount + i.days[j].basePay);
+                });
+                // Daily Personal Tip Amounts
+                if(columnArr[i] > 0){
+                    // Daily Personal Base Pay
+                    // dailyBP.push(tipData[i].days[j].basePay);
+                }
+
+                // Linking Array Identifier with graph line types 
+                if (tipData[i].name === "Daily Total"){ 
+                    chart.data.types[tipData[i].name] = 'bar';
+                }else if (tipData[i].name === "Summer"){
+                    chart.data.types[tipData[i].name] = 'area-spline';
+                }else if(totalsArr[0] === "Total Emp Take"){
+                    chart.data.types[totalsArr[0]] = 'bar';
+                }else {
+                    chart.data.types[tipData[i].name] = 'spline';
+                }
+            }
+        };
+
+        // Push each array uniquely into the Columns array
+        for(var employee in dataArr){
+            columnArr.push(dataArr[employee]);
+        }
+        columnArr.push(totalsArr);
+        chart.data.columns = columnArr;
 }]);
 
--- app/js/directives/graphDirective.js
+++ app/js/directives/graphDirective.js
@@ -1,3 +1,5 @@
+// Danny attempted at breaking apart the c3SimpleService module
+// Fix this soon!
 app.directive("tipGraph", function(c3SimpleService) {
     return {
         restrict: "EA",
